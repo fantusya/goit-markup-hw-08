83f0e9bc2f3f7a75dc1703638ae8486b27c372e6
Anastasiia <guba.anastaysha@gmail.com>
2023-05-04 18:50:54 +0300

    added new features

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,15 +1,23 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector("[data-menu-open]"),
-    closeMenuBtn: document.querySelector("[data-menu-close]"),
-    menu: document.querySelector("[data-menu]"),
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+    mobileList: document.querySelector('.mobile-menu__nav'),
   };
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.mobileList.addEventListener('click', onMobileListClick);
 
   function toggleMenu() {
-    document.body.classList.toggle("menu-open");
-    refs.menu.classList.toggle("is-closed");
+    document.body.classList.toggle('menu-open');
+    refs.menu.classList.toggle('is-closed');
+  }
+
+  function onMobileListClick(e) {
+    if (e.target.classList.contains('mobile-menu__link')) {
+      toggleMenu();
+    }
   }
 })();
--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,38 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', onOpenModal);
+  refs.closeModalBtn.addEventListener('click', onCloseModal);
+  refs.modal.addEventListener('click', onBackdropClick);
 
-  function toggleModal() {
-    document.body.classList.toggle("modal-open");
-    refs.modal.classList.toggle("is-hidden");
+  function onOpenModal() {
+    window.addEventListener('keydown', onEscKeyPress);
+    document.body.classList.add('modal-open');
+    refs.modal.classList.remove('is-hidden');
+  }
+
+  function onCloseModal() {
+    window.removeEventListener('keydown', onEscKeyPress);
+    document.body.classList.remove('modal-open');
+    refs.modal.classList.add('is-hidden');
+  }
+
+  function onBackdropClick(e) {
+    if (e.target === e.currentTarget) {
+      onCloseModal();
+    }
+  }
+
+  function onEscKeyPress(e) {
+    const ESC_KEY_CODE = 'Escape';
+    const isEscPress = e.code === ESC_KEY_CODE;
+
+    if (isEscPress) {
+      onCloseModal();
+    }
   }
 })();
